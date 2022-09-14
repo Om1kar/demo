ba5500a7e324df573856792376f26bfc2d8ac652
omkar <omkardudhewar121@gmail.com>
2022-09-14 13:18:21 +0530

    hshjdh

--- main/java/com/bridgelabz/sample.java
+++ main/java/com/bridgelabz/sample.java
@@ -1,7 +1,10 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class sample {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("hiii...");
     }
 }
